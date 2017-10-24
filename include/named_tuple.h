a18a2ea085d104bffd8c80afaa428756082e2f4a
rbock <rbock@eudoxos.de>
2017-10-24 08:35:43 +0200

    Uncrash MSVC
    See https://developercommunity.visualstudio.com/content/problem/136507/vc-compiler-crashes-with-internal-error-for-this-c.html
    Thanks to Andreas Weis.

--- include/named_tuple.h
+++ include/named_tuple.h
@@ -3,31 +3,37 @@
 #include <tuple>
 #include <utility>
 
-#define NAMED_TUPLE_MEMBER(name, expr)                 \
-  std::make_pair(                                      \
-      [](const auto& base, const auto& index) {        \
-        using base_t = std::decay_t<decltype(base)>;   \
-        using index_t = std::decay_t<decltype(index)>; \
-        struct accessor : public base_t                \
-        {                                              \
-          using base_t::base_t;                        \
-          decltype(auto) name()                        \
-          {                                            \
-            return std::get<index_t::value>(*this);    \
-          }                                            \
-          decltype(auto) name() const                  \
-          {                                            \
-            return std::get<index_t::value>(*this);    \
-          }                                            \
-        };                                             \
-        return accessor{};                             \
-      },                                               \
+#define NAMED_TUPLE_MEMBER(name, expr)                        \
+  std::make_pair(                                             \
+      [](auto wrapped_base, auto index) {                     \
+        using base_t = typename decltype(wrapped_base)::type; \
+        using index_t = decltype(index);                      \
+        struct accessor : public base_t                       \
+        {                                                     \
+          using base_t::base_t;                               \
+          decltype(auto) name()                               \
+          {                                                   \
+            return std::get<index_t::value>(*this);           \
+          }                                                   \
+          decltype(auto) name() const                         \
+          {                                                   \
+            return std::get<index_t::value>(*this);           \
+          }                                                   \
+        };                                                    \
+        return accessor{};                                    \
+      },                                                      \
       expr)
 
 namespace named_tuple
 {
   namespace detail
   {
+    template <typename T>
+    struct wrap
+    {
+      using type = T;
+    };
+
     template <typename Base, size_t index, typename... Names>
     struct named_tuple;
 
@@ -40,12 +46,12 @@ namespace named_tuple
     template <typename Base, size_t index, typename Name, typename... Rest>
     struct named_tuple<Base, index, Name, Rest...>
         : decltype(std::declval<Name>()(
-              std::declval<named_tuple<Base, index + 1, Rest...>>(),
+              wrap<named_tuple<Base, index + 1, Rest...>>{},
               std::integral_constant<size_t, index>{}))
     {
-      using _base = decltype(std::declval<Name>()(
-          std::declval<named_tuple<Base, index + 1, Rest...>>(),
-          std::integral_constant<size_t, index>{}));
+      using _base = decltype(
+          std::declval<Name>()(wrap<named_tuple<Base, index + 1, Rest...>>{},
+                               std::integral_constant<size_t, index>{}));
       using _base::_base;
     };
   }
